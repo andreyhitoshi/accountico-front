5d6a78728f8dd32bfed3a4b5fbaadcf4aa278faf
Anna Flávia <anna.castro@truckpad.com.br>
2020-09-30 04:50:09 -0300

    Removendo códigos que não estão sendo utilizados

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,8 @@
 import React from 'react';
-import './Home.css';
+import PageHome from 'components/User/Home/Home';
 
 const PagesHome = () => (
-  <div className="pages-home">
-    <br />
-    <button type="button">Sair</button>
-  </div>
+  <PageHome />
 );
 
 export default PagesHome;
